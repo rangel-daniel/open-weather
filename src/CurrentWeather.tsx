7b660cd5a067796dc3fbd2bb19aae93bf9a62c44
Daniel Rangel <000.drf@gmail.com>
2023-08-22 21:11:02 -0700

    More CurrentWeather progress

--- src/CurrentWeather.tsx
+++ src/CurrentWeather.tsx
@@ -1,10 +1,12 @@
 import { Current, getAmPm, getDay, getIcon, unixToDate } from "./WeatherData"
-import { BsSunriseFill, BsSunsetFill } from 'react-icons/bs';
+import { BsEyeFill, BsSnow2, BsArrowUp, BsSunriseFill, BsSunsetFill, BsCloudFill, BsCloudRainFill } from 'react-icons/bs';
 import './CurrentWeather.css'
 
 function CurrentWeather({ current }: { current: Current }) {
     const weather = current.weather[0];
+
     const main = current.main;
+    const wind = current.wind;
     const icon = getIcon('l', weather.icon, weather.main);
 
     const sunrise = getAmPm(current.sys.sunrise, current.timezone);
@@ -18,25 +20,93 @@ function CurrentWeather({ current }: { current: Current }) {
     return (
         <div>
             <div id="top">
-                <span><BsSunriseFill /> {sunrise} <br /> sunrise</span>
-                <span>{day} <br /> {time}</span>
+                <span><BsSunriseFill /> {sunrise} <br /> sunrise </span>
+                <span>{day} {time} <h3>{weather.description}</h3> </span>
                 <span> <BsSunsetFill /> {sunset} <br /> sunset</span>
             </div>
+
+            <br />
+
             <div id="main">
-                {icon}
-                <br />
-                <label>
-                    {weather.description}
-                </label>
+                <span id="wind">
+                    <h4>Wind</h4>
+                    <table id="wind-table">
+                        <tr>
+                            <td className="col">speed</td>
+                            <td className="row">{wind.speed}</td>
+                        </tr>
+                        <tr>
+                            <td className="col">degree</td>
+                            <td className="row">
+                                <span style={{ display: 'inline-block', transform: `rotate(${wind.deg}deg)` }}>
+                                    <BsArrowUp />
+                                </span>
+                                &nbsp;{wind.deg}&deg;
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="col">gust</td>
+                            <td className="row">{wind.gust}</td>
+                        </tr>
+                    </table>
+
+                </span>
+
+                <div id="icon-temp">
+                    {icon}
+                    <h4> {main.temp}</h4>
+                </div>
+
+                <span id="temp">
+                    <h4>Temperature</h4>
+                    <table id="temp-table">
+                        <tr>
+                            <td className="col">feels like</td>
+                            <td className="row">{main.feels_like}</td>
+                        </tr>
+                        <tr>
+                            <td className="col">min</td>
+                            <td className="row">{main.temp_min}</td>
+                        </tr>
+                        <tr>
+                            <td className="col">max</td>
+                            <td className="row">{main.temp_max}</td>
+                        </tr>
+                    </table>
+                </span>
             </div>
-            <div id="temp">
-                <div>
-                    {main.temp}&deg;F
-                </div>
-                <div>
-                    {main.feels_like}&deg;F
-                    <br /> feels like
-                </div>
+            <div id="misc">
+                <p>
+                    <p>
+                        <b>Sea level</b>: {main.sea_level}<b>hPa</b>
+                    </p>
+                    <p>
+                        <b>Ground level</b>: {main.grnd_level}<b>hPa</b>
+                    </p>
+                    <p>
+                        <b>Pressure</b>: {main.grnd_level}<b>hPa</b>
+                    </p>
+                    <p>
+                        <b>Humidity</b>: {main.humidity}%
+                    </p>
+                </p>
+                <p>
+                    {current.snow ? (
+                        <p>
+                            <BsSnow2 />
+                        </p>
+                    ) : current.rain ? (
+                        <p>
+                            <BsCloudRainFill title='rain volume' /> {current.rain["1h"]}mm/hr
+                        </p>
+                    ) : undefined}
+                    <p>
+                        <BsCloudFill title='cloudiness' /> {current.clouds.all}%
+                    </p>
+                    <p>
+                        <BsEyeFill title='visibility' /> {current.visibility / 1000}km
+                    </p>
+                </p>
             </div>
         </div>
 
--- src/Weather.tsx
+++ src/Weather.tsx
@@ -2,26 +2,26 @@ import { Properties } from 'csstype'
 import { useEffect, useState } from 'react';
 import { Place } from './App';
 import './Weather.css'
-import { Current, FiveDay } from './WeatherData';
+import { Current } from './WeatherData';
 
 import CurrentWeather from './CurrentWeather';
 
 const APPID = import.meta.env.VITE_APPID;
 
 function Weather({ place }: { place?: Place }) {
-    const [is_curr, setIsCurr] = useState(false);
+    const [is_curr, setIsCurr] = useState(true);
+    const [is_imp, setIsImp] = useState(true);
 
     const [current, setCurrent] = useState<Current>();
-    const [five_day, setFiveDay] = useState<FiveDay>();
 
     const l_bg: Properties = {
-        backgroundColor: is_curr ? 'transparent' : '#3584e4',
-        color: is_curr ? 'black' : 'white'
+        backgroundColor: !is_curr ? 'transparent' : '#3584e4',
+        color: !is_curr ? 'black' : 'white'
     }
 
     const r_bg: Properties = {
-        backgroundColor: !is_curr ? 'transparent' : '#3584e4',
-        color: !is_curr ? 'black' : 'white'
+        backgroundColor: is_curr ? 'transparent' : '#3584e4',
+        color: is_curr ? 'black' : 'white'
     }
 
     useEffect(() => {
@@ -41,10 +41,16 @@ function Weather({ place }: { place?: Place }) {
 
     return (
         <div id='weather-box' >
-            <label id="selector">
-                <div id="left" onClick={() => setIsCurr(false)} style={l_bg}>5 day</div>
-                <div id="right" onClick={() => setIsCurr(true)} style={r_bg}>Current</div>
-            </label>
+            <div id='options'>
+                <div id="mode">
+                    <div id="left" onClick={() => setIsCurr(true)} style={l_bg}>Current</div>
+                    <div id="right" onClick={() => setIsCurr(false)} style={r_bg}>5 day</div>
+                </div>
+                <span id="unit">
+                    <span id="imperial" onClick={() => setIsImp(true)} style={{ fontWeight: is_imp ? 'bold' : 'lighter' }}>F&deg;</span>
+                    <span id="metric" onClick={() => setIsImp(false)} style={{ fontWeight: is_imp ? 'lighter' : 'bold' }}>C&deg;</span>
+                </span>
+            </div>
 
             <div id='location-outer'>
                 <div id='location'>
