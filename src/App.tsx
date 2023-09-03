bfc61193368e904fab2faa35ee95c011020dd4f9
Daniel Rangel <000.drf@gmail.com>
2023-09-02 23:44:49 -0700

    Attempt to fix image not displaying

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
         <>
             <header>
                 <a href='https://openweathermap.org/' draggable='false'>
-                    <img src='/open_weather.png' alt='open weather' draggable='false' />
+                    <img src='./open_weather.png' alt='open weather' draggable='false' />
                 </a>
             </header>
 
